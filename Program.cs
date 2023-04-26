9762ab3a4aa40f2a19924900031462e8ec25402e
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-04-26 16:24:49 +0545

    Message changed

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 namespace Internship{
     public class Application{
         public static void Main(string[] args){
-            Console.WriteLine("Welcome!");
+            Console.WriteLine("Welcome, guys!");
         }
     }
 }
