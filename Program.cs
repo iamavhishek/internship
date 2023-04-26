10b89c0f88a128503398a29562888524527a5ee4
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-04-26 17:11:29 +0545

    Main
    reBranch

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 namespace Internship{
     public class Application{
         public static void Main(string[] args){
-            Console.WriteLine("Hi, I'm Ravi!");
+            Console.WriteLine("Hi");
+            Console.WriteLine("Hi-Rebase");
+            Console.WriteLine("Hi-Squash");
         }
     }
 }
