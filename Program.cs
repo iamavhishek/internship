23c1ef97f379fcbac956f95c18fc4e51c1c81634
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-04-26 15:49:01 +0545

    Changed content of Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Internship{
+    public class Application{
+        public static void Main(string[] args){
+            Console.WriteLine("Welcome to Github!");
+        }
+    }
+}
