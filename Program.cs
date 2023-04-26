bb15222db02aea6e7a3f5a586cb91db031e8e722
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-04-26 17:11:29 +0545

    Fresh Start By Menu

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 namespace Internship{
     public class Application{
         public static void Main(string[] args){
-            Console.WriteLine("Hi, I'm Ravi!");
+            Console.WriteLine("Hi");
         }
     }
 }
