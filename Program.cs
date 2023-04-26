2ffc497b8ed1c351a57e5864ee497cb979d3f87f
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-04-26 15:58:31 +0545

    Modified Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 namespace Internship{
     public class Application{
         public static void Main(string[] args){
-            Console.WriteLine("Welcome to Github!");
+            Console.WriteLine("Welcome!");
         }
     }
 }
