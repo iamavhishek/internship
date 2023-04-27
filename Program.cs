318dfe5d4b02b218e31f4077f65f3d4fbab96764
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-04-27 13:31:44 +0545

    Finished By Main

--- Program.cs
+++ Program.cs
@@ -2,8 +2,7 @@
 namespace Internship{
     public class Application{
         public static void Main(string[] args){
-            Console.WriteLine("Hi I'm Ravi!");
-            Console.WriteLine("Cherry");
+            Console.WriteLine("Hi, I'm Ravi!");
         }
     }
 }
